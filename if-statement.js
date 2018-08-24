bd8e09c1a550da3e5fcc0373f90301ed666c291a
Suganthi Jayaseelan <suganthi1483@gmail.com>
2018-08-24 07:08:09 +0000

    Add existing file

---
+++ if-statement.js
@@ -0,0 +1,11 @@
+var fruit;
+fruit = 'orange';
+fruitname = fruit.length;
+if (fruitname > 5)
+{
+  console.log('Fruit has more than 5 characters');
+}
+else {
+  console.log('Fruit has less than 5 characters');
+
+}
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+var pizza = 'pizza is alright';
+pizza = pizza.replace('alright','is awesome');
+console.log(pizza);
